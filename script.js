6726c4e189aaae1f92664baa84313f6f6bce7f25
Samuel Akongbota <122745519+Reysam2@users.noreply.github.com>
2025-08-09 20:47:44 +0000

    feat: add logic to save todos to local storage
    Added a feature that helps store user data after an ended session to
    local storage. This helps prevent user data loss after the page closes
    or refreshes enabling restoration of user todo data right from the local
    storage.

--- script.js
+++ script.js
@@ -5,8 +5,39 @@ document.addEventListener('DOMContentLoaded', () => {
   const taskInput = document.querySelector('#task-input');
   const addButton = document.querySelector('#add-task-btn');
   const taskList = document.querySelector('#task-list');
+  let todoArray = JSON.parse(localStorage.getItem('Todos')) || []
 
 
+  // create li and remove button logic
+  function loadTask(taskText) {
+
+    // create li element
+    const listItem = document.createElement('li');
+    listItem.textContent = taskText;
+
+    // add class to li
+    listItem.classList.add('li');
+    taskList.appendChild(listItem);
+
+
+    // create a remove button 
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    // add class to button
+    removeButton.classList.add('remove-btn');
+
+    listItem.appendChild(removeButton);
+
+    // add an event to execute upon click
+    removeButton.addEventListener('click', () => {
+      taskList.removeChild(listItem);
+      todoArray = todoArray.filter((todo) => todo !== taskText);
+      localStorage.setItem('Todos', JSON.stringify(todoArray))
+
+    })
+  }
+
+  // function that stores to local storage and retrieves data from memory
   function addTask() {
     const taskText = taskInput.value.trim();
 
@@ -17,31 +48,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     } else {
 
-
-      // create li element
-      const listItem = document.createElement('li');
-      listItem.textContent = taskText;
-
-      // add class to li
-      listItem.classList.add('li');
-      taskList.appendChild(listItem);
-
-
-      // create a remove button 
-      const removeButton = document.createElement('button');
-      removeButton.textContent = 'Remove';
-      // add class to button
-      removeButton.classList.add('remove-btn');
-
-      listItem.appendChild(removeButton);
-
-      // add an event to execute upon click
-      removeButton.addEventListener('click', () => {
-        taskList.removeChild(listItem)
-      })
-
+      loadTask(taskText)
       taskInput.value = "";
 
+
+      // function that retrieves from memory
+      function toMemory() {
+        todoArray.push(taskText);
+
+        // changing to json
+        let todoListJson = JSON.stringify(todoArray);
+
+        // storing to memory
+        localStorage.setItem('Todos', todoListJson);
+
+      }
+
+      toMemory()
 
 
     }
@@ -51,6 +74,15 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 
+
+  // loops through the restored data and appends to the page
+  for (let todo of todoArray) {
+    loadTask(todo)
+
+  }
+
+
+
   addButton.addEventListener('click', (e) => {
     e.preventDefault();
 
@@ -58,14 +90,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
   })
 
-  taskInput.addEventListener('keypress', function(event) {
+  taskInput.addEventListener('keypress', function (event) {
 
     if (event.key === 'Enter') {
       event.preventDefault();
       addTask()
     }
   })
- 
+
 
 
 
